05e228b5dcb6c7e099f6f07a30fc7d16506707af
Alexey Golub <tyrrrrrr@gmail.com>
2015-10-09 13:55:31 +0300

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Steam Market Excerpt ===
+Author: Alexey 'Tyrrrz' Golub
+Homepage: http://www.tyrrrz.me
+
+== Description ==
+Steam Market Excerpt (SME) plugin is used to add Steam Market item listings to your Wordpress posts. It shows and automatically updates the item's name, image, lowest listed price, median price and total number of listings for the given item.
+This plugin is utilized via a special shortcode - [steam_market_excerpt]
+
+== Installation ==
+Go to Wordpress Dashboard -> Plugins -> Add New. Press Upload Plugin and navigate to the .zip file of the plugin.
+--or--
+Copy the contents of the .zip file to /your_wp_blog_ftp_location/wp-content/plugins/
+
+== Usage ==
+This plugin is utilized via its own shortcode.
+
+The minimal format for the shortcode looks like this:
+[steam_market_excerpt game="730" name="AK-47 | Elite Build (Factory New)"]
+Where game="730" defines Steam appId of a particular game (730 is CS:GO). You can look up game IDs in the store by checking the URL of their store pages.
+Name should specify the name of the item with correct amount of spaces, preserving all the special symbols and casing. You can copy-paste the name from Steam Market listing to make sure you don't miss anything.
+
+Full specification of the shortcode is as follows:
+[steam_market_excerpt game="" name="" displayname="" currency="1" showimage="true" imgwidth="240" imgheight="240"]
+Game and Name are required and must always be present.
+Displayname can be altered to change the displayed name of the item, without affecting it's reference to the actual item.
+Currency defines the currency for lowest price and median price fields. They are assigned using their unique IDs. ID=1 belongs to US dollars, which is default.
+Showimage defines whether the item image should be downloaded. If set to false, it can save up considerable amount of bandwith. It will also not generate the respective <div> fragment for the image if set to false.
+Imgwidth and Imgheight defines the maximum image dimensions that will be queried from the Steam servers. Has no effect if Showimage is set to false. Reducing the values can potentially reduce bandwith.
+
+== Styling ==
+It's really easy to change how each part of the excerpt looks. To do that, you need to locate the file called style.css that's located in /wp-content/plugins/SteamMarketExcerpt/assets/css/style.css by default. Each part of the excerpt has its own class that you can use to write your own CSS styles for it.
+
+== Contact ==
+If you, for any reason, want to contact me - use my contact form at http://www.tyrrrz.me .
+Thanks for using my plugin, hope you enjoy!
---
+++ steam-market-excerpt.php
@@ -0,0 +1,123 @@
+<?php
+/*
+Plugin Name: Steam Market Excerpt
+Plugin URI:  http://www.tyrrrz.me/projects/wpsme.php
+Description: This plugin is used to make excerpts of Steam Market item listings
+Version:     1.2
+Author:      Alexey Golub
+Author URI:  http://www.tyrrrz.me
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Abspath
+defined('ABSPATH') or die('RIP skins');
+
+// Disable warnings
+error_reporting(E_ERROR);
+
+// Shortcode function
+function steam_market_excerpt_shortcode($atts) {
+	// Get attributes
+	$a = shortcode_atts(array(
+        'game' => null,
+        'name' => null,
+        'displayname' => null,
+        'currency' => 1,
+        'showimage' => true,
+        'imgwidth' => 240,
+        'imgheight' => 240
+    ), $atts);	
+
+	// Save attributes to local vars
+    $game = $a['game'];
+    $name = $a['name'];
+    $displayname = $a['displayname'];
+    $currency = $a['currency'];
+    $showimage = $a['showimage'];
+    $imgwidth = $a['imgwidth'];
+    $imgheight = $a['imgheight'];
+
+    // Make sure everything is set
+    if (!isset($game) or !isset($name)) return '<div class="steam-market-excerpt-error">Game and Name parameters must be set</div>';
+
+    // Set display name
+    if (!isset($displayname))
+    	$displayname = $name;
+
+    // Query steam market API
+    $url =
+        'http://steamcommunity.com/market/priceoverview/'.
+        '?appid='.$game.
+        '&currency='.$currency.
+        '&market_hash_name='.rawurlencode($name);
+    $response = file_get_contents($url);
+    $json = json_decode($response);
+
+    // Determine success
+    if (!$json->success) return '<div class="steam-market-excerpt-error">No listings found for this item</div>';
+
+    // Store results
+    $lowest_price = $json->lowest_price;
+    $median_price = $json->median_price;
+    $volume = $json->volume;
+
+    // Query the render to find the image src
+    $market_page_url = 'http://steamcommunity.com/market/listings/'.$game.'/'.rawurlencode($name);
+    $imgurl = null;
+    if ($showimage) {
+        // Get the API query that returns the render html
+        $url = $market_page_url.'/render?start=0&count=1&currency='.$currency.'format=json';
+        $response = file_get_contents($url);
+        $render_html = json_decode($response)->results_html;
+
+        // Parse the html
+        $dom = new DOMDocument;
+        $dom->loadHTML($render_html);
+        $xpath = new DOMXpath($dom);
+
+        // Get the image
+        $img = $xpath->query('//img[@class="market_listing_item_img"]')->item(0);
+        if (isset($img)) {
+        	// Get image src
+	        $imgurl = $img->getAttribute('src');
+
+	        // Remove the pre-set size
+	        $imgurl = substr($imgurl, 0, strrpos($imgurl, '/'));
+
+	        // Replace with our size
+	        $imgurl .= '/'.$imgwidth.'fx'.$imgheight.'f';
+    	}
+    }
+
+    // Output result
+    $output = '';
+    $output .= '<div class="steam-market-excerpt">';
+    $output .= '<div class="steam-market-excerpt-name"><a class="steam-market-excerpt-name" target="_blank" href="'.$market_page_url.'">'.$displayname.'</a></div>';
+    if ($showimage and isset($imgurl))
+        $output .= '<div class="steam-market-excerpt-image"><img class="steam-market-excerpt-image" src="'.$imgurl.'"/></div>';
+    $output .= '<div class="steam-market-excerpt-fields">';
+    if (isset($lowest_price))
+    	$output .= '<div class="steam-market-excerpt-lowestprice">Lowest Price: '.$lowest_price.'</div>';
+    if (isset($median_price))
+    	$output .= '<div clas="steam-market-excerpt-medianprice">Median Price: '.$median_price.'</div>';
+    if (isset($volume))
+    	$output .= '<div class="steam-market-excerpt-volume">Volume: '.$volume.'</div>';
+    $output .= '</div>';
+    $output .= '</div>';
+    return $output;
+}
+
+// Plugin load function
+function steam_market_excerpt_load() {
+    wp_register_style('steam-market-excerpt-style', plugin_dir_url(__FILE__).'assets/css/style.css');
+    wp_enqueue_style('steam-market-excerpt-style');
+}
+
+
+// Init
+add_action('wp_enqueue_scripts', 'steam_market_excerpt_load');
+
+// Shortcodes
+add_shortcode('steam_market_excerpt', 'steam_market_excerpt_shortcode');
+?>
